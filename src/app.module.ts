13d92fd91eecce3c335bb303e5275b967c17a7f1
Cristian Salerno <cristiansalerno.18@gmail.com>
2022-01-26 16:49:46 +0100

    swagger config and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
-import { TasksController } from './tasks/tasks.controller';
-import { TasksService } from './tasks/tasks.service';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TasksModule,
-    MongooseModule.forRoot('mongodb://localhost/nest-tasks'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB),
   ],
   controllers: [AppController, UserController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestJs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Swagger settings
+  const options = new DocumentBuilder()
+    .setTitle('Tasks')
+    .setDescription('Task descriptions')
+    .setVersion('1.0')
+    .addTag('Tasks')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
