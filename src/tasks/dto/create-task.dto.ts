48fa1db446724938522d2570606f72a995296e29
Cristian Salerno <cristiansalerno.18@gmail.com>
2022-01-26 18:38:36 +0100

    swagger descriptions added

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateTaskDto {
+  @ApiProperty({
+    type: String,
+    description: 'Task Title',
+  })
   title: string;
+  @ApiProperty({
+    type: String,
+    description: 'Task Description',
+  })
   description: string;
+  @ApiProperty({
+    type: Boolean,
+    description: 'Task is Done or not',
+  })
   done: boolean;
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -13,27 +13,44 @@ import {
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './interfaces/Task';
 import { TasksService } from './tasks.service';
+import { ApiCreatedResponse, ApiResponse } from '@nestjs/swagger';
 
 @Controller('task')
 export class TasksController {
   constructor(private readonly taskService: TasksService) {}
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Get all the Tasks',
+  })
   getTasks(): Promise<Task[]> {
     return this.taskService.getTasks();
   }
 
   @Get(':taskId')
+  @ApiResponse({
+    status: 200,
+    description: 'Get a Task by Task Id',
+  })
   getTask(@Param('taskId') taskId: string): Promise<Task> {
     return this.taskService.getTaskById(taskId);
   }
 
   @Post()
+  @ApiResponse({
+    status: 200,
+    description: 'Create a new task',
+  })
   createTask(@Body() task: CreateTaskDto): Promise<Task> {
     return this.taskService.createTask(task);
   }
 
   @Patch(':taskId')
+  @ApiResponse({
+    status: 200,
+    description: 'Find and update a task, require Id and Task',
+  })
   updateTask(@Req() req, @Param('taskId') taskId): string {
     this.taskService.updateTask(taskId, req.body);
 
@@ -41,6 +58,10 @@ export class TasksController {
   }
 
   @Delete(':taskId')
+  @ApiResponse({
+    status: 200,
+    description: 'Delete a task',
+  })
   deleteTask(@Param('taskId') taskId): string {
     this.taskService.deleteTask(taskId);
     return `task deleted correctly, task id: ${taskId}`;
